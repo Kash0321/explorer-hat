42ccee630ec5fa3159168b9c4fe3e20838d27365
kash <castillo.sergio@gmail.com>
2019-12-29 16:50:01 +0100

    Obstacle avoiding completed

---
+++ samples/ExplorerHat.ObstacleAvoidance/Hcsr04Sonar.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Device.Gpio;
+using System.Threading;
+using System.Device.Gpio.Drivers;
+
+namespace ExplorerHat.ObstacleAvoidance
+{
+    /// <summary>
+    /// HC-SR04 - Ultrasonic Ranging Module
+    /// </summary>
+    public class Hcsr04Sonar : IDisposable
+    {
+        private readonly int _echo;
+        private readonly int _trigger;
+        private GpioController _controller;
+        private Stopwatch _timer = new Stopwatch();
+
+        private int _lastMeasurment = 0;
+
+        /// <summary>
+        /// Gets the current distance in cm.
+        /// </summary>
+        public double Distance => GetDistance();
+
+        /// <summary>
+        /// Creates a new instance of the HC-SCR04 sonar.
+        /// </summary>
+        /// <param name="triggerPin">Trigger pulse input.</param>
+        /// <param name="echoPin">Trigger pulse output.</param>
+        /// <param name="pinNumberingScheme">Pin Numbering Scheme</param>
+        public Hcsr04Sonar(int triggerPin, int echoPin, PinNumberingScheme pinNumberingScheme = PinNumberingScheme.Logical)
+        {
+            _echo = echoPin;
+            _trigger = triggerPin;
+            _controller = new GpioController(pinNumberingScheme, new LibGpiodDriver());
+
+            _controller.OpenPin(_echo, PinMode.Input);
+            _controller.OpenPin(_trigger, PinMode.Output);
+            
+            _controller.Write(_trigger, PinValue.Low);
+        }
+
+        /// <summary>
+        /// Gets the current distance in cm.
+        /// </summary>
+        private double GetDistance()
+        {
+            _timer.Reset();
+
+            // Measurements should be 60ms apart, in order to prevent trigger signal mixing with echo signal
+            // ref https://components101.com/sites/default/files/component_datasheet/HCSR04%20Datasheet.pdf
+            while (Environment.TickCount - _lastMeasurment < 60)
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(Environment.TickCount - _lastMeasurment));
+            }
+
+            // Trigger input for 10uS to start ranging
+            _controller.Write(_trigger, PinValue.High);
+            var waitTime = TimeSpan.FromTicks(10);
+            Thread.Sleep(waitTime);
+            _controller.Write(_trigger, PinValue.Low);
+
+            // Wait until the echo pin is HIGH (that marks the beginning of the pulse length we want to measure)
+            while (_controller.Read(_echo) == PinValue.Low)
+            {
+            }
+
+            _lastMeasurment = Environment.TickCount;
+
+            _timer.Start();
+
+            // Wait until the pin is LOW again, (that marks the end of the pulse we are measuring)
+            while (_controller.Read(_echo) == PinValue.High)
+            {
+            }
+
+            _timer.Stop();
+
+            TimeSpan elapsed = _timer.Elapsed;
+
+            // distance = (time / 2) × velocity of sound (34300 cm/s)
+            return elapsed.TotalMilliseconds / 2.0 * 34.3;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (_controller != null)
+            {
+                _controller.Dispose();
+                _controller = null;
+            }
+        }
+    }
+}
--- samples/ExplorerHat.ObstacleAvoidance/Program.cs
+++ samples/ExplorerHat.ObstacleAvoidance/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Serilog;
 using System;
-using System.Device.Gpio;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -8,15 +7,13 @@ namespace ExplorerHat.ObstacleAvoidance
 {
     class Program
     {
-        const int TRIG = 6;
-        const int ECHO = 23;
         static void Main(string[] args)
         {
-            // Logging configuration
+            //Logging configuration
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .WriteTo.Console()
-                .CreateLogger();
+               .MinimumLevel.Verbose()
+               .WriteTo.Console()
+               .CreateLogger();
 
             // Priting OS INFO
             Console.WriteLine("**************************************************************************************");
@@ -28,43 +25,15 @@ namespace ExplorerHat.ObstacleAvoidance
 
             using (var hat = new Iot.Device.ExplorerHat.ExplorerHat())
             {
-                var pulseStart = DateTime.MinValue;
-                var pulseEnd = DateTime.MinValue;
-                var pulseDuration = 0d;
-                var distance = 0d;
-                var gpio = Iot.Device.ExplorerHat.Gpio.GpioController.Current;
+                var distance = SonarSingleton.Distance;
 
                 Log.Information("Medición de distancia en progreso...");
 
-                Iot.Device.ExplorerHat.Gpio.GpioController.EnsureOpenPin(TRIG, PinMode.Output);
-                Iot.Device.ExplorerHat.Gpio.GpioController.EnsureOpenPin(ECHO, PinMode.Input);
-
-                Log.Information("Esperando a que el sensor se inicialice...");
-
-                Thread.Sleep(1000);
-
                 Log.Information("Arrancamos! Motores al 70%");
                 hat.Motors.Forwards(0.7);
-
                 while (true)
                 {
-                    gpio.Write(TRIG, PinValue.High);
-                    Thread.Sleep(TimeSpan.FromMilliseconds(0.01));
-                    gpio.Write(TRIG, PinValue.Low);
-
-                    // while (gpio.Read(ECHO) == 0)
-                    // {
-                    //    pulseStart = DateTime.Now;
-                    // }
-
-                    // while (gpio.Read(ECHO) == 1)
-                    // {
-                    //    pulseEnd = DateTime.Now;
-                    // }
-
-                    pulseDuration = (pulseEnd - pulseStart).TotalMilliseconds;
-
-                    distance = Math.Round(pulseDuration * 17150, 2, MidpointRounding.AwayFromZero);
+                    distance = SonarSingleton.Distance;
 
                     if (distance < 40d)
                     {
@@ -73,22 +42,22 @@ namespace ExplorerHat.ObstacleAvoidance
                         hat.Lights.Three.On();
                         hat.Lights.Four.On();
 
-                        // Maniobras para evitar un obstáculo
-                        // Log.Information("Obstáculo detectado. Ejecutando maniobras para evitarlo...");
-                        // hat.Motors.Stop();
-                        // Log.Information("Motores parados");
-                        // hat.Motors.Backwards(0.6);
-                        // Log.Information("Atrás...");
-                        // Thread.Sleep(TimeSpan.FromMilliseconds(0.5));
-                        // Log.Information("Girando...");
-                        // hat.Motors.One.Forwards(0.6);
-                        // hat.Motors.Two.Backwards(0.6);
-                        // Thread.Sleep(TimeSpan.FromMilliseconds(0.75));
-                        // Log.Information("Giro completado");
-                        // Log.Information("Motores al 70%");
-                        // hat.Motors.Forwards(0.7);
+                        //Maniobras para evitar un obstáculo
+                        Log.Information("Obstáculo detectado. Ejecutando maniobras para evitarlo...");
+                        hat.Motors.Stop();
+                        Log.Information("Motores parados");
+                        hat.Motors.Backwards(0.6);
+                        Log.Information("Atrás...");
+                        Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                        Log.Information("Girando...");
+                        hat.Motors.One.Forwards(0.6);
+                        hat.Motors.Two.Backwards(0.6);
+                        Thread.Sleep(TimeSpan.FromSeconds(0.75));
+                        Log.Information("Giro completado");
+                        Log.Information("Motores al 70%");
+                        hat.Motors.Forwards(0.7);
                     }
-                    else if (distance < 60d )
+                    else if (distance < 60d)
                     {
                         Log.Information("Motores al 65%");
                         hat.Motors.Forwards(0.65);
@@ -97,7 +66,7 @@ namespace ExplorerHat.ObstacleAvoidance
                         hat.Lights.Three.On();
                         hat.Lights.Four.Off();
                     }
-                    else if (distance < 120d )
+                    else if (distance < 120d)
                     {
                         Log.Information("Motores al 70%");
                         hat.Motors.Forwards(0.70);
@@ -106,7 +75,7 @@ namespace ExplorerHat.ObstacleAvoidance
                         hat.Lights.Three.Off();
                         hat.Lights.Four.Off();
                     }
-                    else if (distance < 240d )
+                    else if (distance < 240d)
                     {
                         Log.Information("Motores al 75%");
                         hat.Motors.Forwards(0.75);
@@ -125,8 +94,8 @@ namespace ExplorerHat.ObstacleAvoidance
                         hat.Lights.Four.Off();
                     }
 
-                    Log.Information($"Distancia al obstáculo más cercano: {distance} cm [{pulseStart.ToShortDateString()} {pulseStart.ToShortTimeString()}/{pulseEnd.ToShortDateString()} {pulseEnd.ToShortTimeString()}]");
-                    Thread.Sleep(TimeSpan.FromMilliseconds(0.2));
+                    Log.Information($"Distancia al obstáculo más cercano: {distance} cm.");
+                    Thread.Sleep(TimeSpan.FromSeconds(0.2));
                 }
             }
         }
